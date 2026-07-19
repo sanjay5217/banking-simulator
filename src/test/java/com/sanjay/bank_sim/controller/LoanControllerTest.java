5a5fdc41f17b780a4d1304f77588694a44f2dd85
sanjay <sanjay_ram2007@outlook.com>
2026-07-19 01:17:01 -0600

    fixed a test

--- src/test/java/com/sanjay/bank_sim/controller/LoanControllerTest.java
+++ src/test/java/com/sanjay/bank_sim/controller/LoanControllerTest.java
@@ -15,7 +15,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
