da5e7970b1614514a252e4be9bb20f99cac892d1
sanjay <sanjay_ram2007@outlook.com>
2026-07-19 01:22:09 -0600

    CI config

--- src/test/java/com/sanjay/bank_sim/BankSimApplicationTests.java
+++ src/test/java/com/sanjay/bank_sim/BankSimApplicationTests.java
@@ -2,12 +2,29 @@ package com.sanjay.bank_sim;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 @SpringBootTest
+@Testcontainers
 class BankSimApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
 
+    @Test
+    void contextLoads() throws Exception {
+        try (Connection conn = DriverManager.getConnection(
+                postgres.getJdbcUrl(), postgres.getUsername(), postgres.getPassword())) {
+            ScriptUtils.executeSqlScript(conn, new ClassPathResource("queries/schema.sql"));
+        }
+    }
 }
